0d1fd6ea1e0631b003b2f169de4eacde0aa6ee82
Franz Alarcon <franzalarconv@gmail.com>
2024-01-18 13:52:41 -0800

    Add support for input modifiers

--- src/MacroModules.MacroLibrary/Types/InputData.cs
+++ src/MacroModules.MacroLibrary/Types/InputData.cs
@@ -1,5 +1,6 @@
 ﻿using MacroModules.MacroLibrary.WinApi;
 using static MacroModules.MacroLibrary.WinApi.HookingApi;
+using static MacroModules.MacroLibrary.WinApi.KeyStateApi;
 using static MacroModules.MacroLibrary.WinApi.SendInputApi;
 
 namespace MacroModules.MacroLibrary.Types
@@ -14,6 +15,14 @@ namespace MacroModules.MacroLibrary.Types
         HorizontalScroll
     }
 
+    [Flags]
+    public enum InputModifiers
+    {
+        ShiftHeld = 1,
+        CtrlHeld = 1 << 1,
+        AltHeld = 1 << 2
+    }
+
     /// <summary>
     /// Represents the data associated with an input event.
     /// </summary>
@@ -23,6 +32,11 @@ namespace MacroModules.MacroLibrary.Types
         /// The type of the input event. 
         /// </summary>
         public InputType Type { get; private set; }
+
+        /// <summary>
+        /// Flags that indicate if any modifier keys were being held during the input.
+        /// </summary>
+        public InputModifiers Modifiers { get; private set; }
 
         /// <summary>
         /// The time the input occurred in milliseconds since the system started.
@@ -58,6 +72,7 @@ namespace MacroModules.MacroLibrary.Types
         {
             Timestamp = data.time;
             InputKeyCode = (ushort)data.vkCode;
+            SetModifierFlags();
 
             if (messageType == KeyboardMessage.KeyDown || messageType == KeyboardMessage.SysKeyDown)
             {
@@ -70,6 +85,7 @@ namespace MacroModules.MacroLibrary.Types
         internal InputData(MouseMessage messageType, MouseHookStruct data)
         {
             Timestamp = data.time;
+            SetModifierFlags();
 
             switch (messageType)
             {
@@ -120,6 +136,33 @@ namespace MacroModules.MacroLibrary.Types
                 default:
                     Type = InputType.Invalid;
                     return;
+            }
+        }
+
+        private int VK_SHIFT = 0x10;
+        private int VK_CONTROL = 0x11;
+        private int VK_MENU = 0x12;
+
+        private void SetModifierFlags()
+        {
+            // To check if an input is being held down, the high-order bit needs to be checked.
+            // This is done by unsigned shifting the bit right by 31 bits so that the high order
+            // bit is at the low-order position. Then just check if the value is 1.
+
+            // The return value of GetKeyState is a short, which is 16 bits. But we need to shift
+            // by 31 bits since the short gets casted to an int (32 bits) when doing bit shifts.
+
+            if (GetKeyState(VK_SHIFT) >>> 31 == 1)
+            {
+                Modifiers |= InputModifiers.ShiftHeld;
+            }
+            if (GetKeyState(VK_CONTROL) >>> 31 == 1)
+            {
+                Modifiers |= InputModifiers.CtrlHeld;
+            }
+            if (GetKeyState(VK_MENU) >>> 31 == 1)
+            {
+                Modifiers |= InputModifiers.AltHeld;
             }
         }
     }

90ab76266d890023abb65dc0c7bb2093f77d1534
Franz Alarcon <franzalarconv@gmail.com>
2024-01-29 14:35:01 -0800

    Add docs to interface

--- src/MacroModules.Model/Variable/IVariableValues.cs
+++ src/MacroModules.Model/Variable/IVariableValues.cs
@@ -1,5 +1,11 @@
 ﻿namespace MacroModules.Model.Variable
 {
+    /// <summary>
+    /// Defines value properties for macro variables.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type of value that will be stored by the macro variable.
+    /// </typeparam>
     public interface IVariableValues<T>
     {
         /// <summary>

a6ba96dcce9f4702d8beffd7aa8ddde4e71878e1
Franz Alarcon <franzalarconv@gmail.com>
2024-01-29 14:15:53 -0800

    Implement string-type VariableBase and IVariableValues

--- src/MacroModules.Model/Variable/StringVariable.cs
+++ src/MacroModules.Model/Variable/StringVariable.cs
@@ -1,12 +1,15 @@
 ﻿namespace MacroModules.Model.Variable
 {
-    internal class StringVariable : VariableBase, IVariableValues<string>
+    /// <summary>
+    /// Represents a string macro variable.
+    /// </summary>
+    public class StringVariable : VariableBase, IVariableValues<string>
     {
         public override VariableType Type { get; } = VariableType.String;
 
-        public string InitialValue { get; set; } = "";
+        public string InitialValue { get; set; } = string.Empty;
 
-        public string RuntimeValue { get; set; } = "";
+        public string RuntimeValue { get; set; } = string.Empty;
 
         public override void InitializeRuntimeValue()
         {
--- src/MacroModules.Model/Variable/VariableFactory.cs
+++ src/MacroModules.Model/Variable/VariableFactory.cs
@@ -29,7 +29,7 @@
         /// </summary>
         private static Dictionary<VariableType, Func<VariableBase>> variableFactories = new()
         {
-            { VariableType.String, () => new VariableTest() }
+            { VariableType.String, () => new StringVariable() }
         };
     }
 }

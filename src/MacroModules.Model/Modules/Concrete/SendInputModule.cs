2d6aabb4f18a30118ce9febdee2c649952e0a008
Franz Alarcon <franzalarconv@gmail.com>
2024-02-01 13:27:41 -0800

    Implement SendInputModule

---
+++ src/MacroModules.Model/Modules/Concrete/SendInputModule.cs
@@ -0,0 +1,51 @@
+﻿using MacroModules.MacroLibrary;
+using MacroModules.Model.Modules.Responses;
+
+namespace MacroModules.Model.Modules.Concrete
+{
+    public enum SendInputAction
+    {
+        Click,
+        Hold,
+        Release
+    }
+
+    public class SendInputModule : Module
+    {
+        /// <summary>
+        /// Indicates the virtual key code of the input to send.
+        /// </summary>
+        /// <remarks>
+        /// This module uses the defined Windows virtual key codes to send inputs. A defined set of
+        /// most key code can be selected from <see cref="MacroLibrary.Types.InputCode"/>.
+        /// </remarks>
+        public ushort InputCode { get; set; } = 0;
+
+        /// <summary>
+        /// Indicates the input action to perform. This may be a click, hold, or release action.
+        /// </summary>
+        public SendInputAction Action { get; set; } = SendInputAction.Click;
+
+        public override ModuleType Type { get; } = ModuleType.SendInput;
+
+        public override IResponse Execute(ref object? processData)
+        {
+            switch (Action)
+            {
+                case SendInputAction.Click:
+                    InputControl.Click(InputCode);
+                    break;
+
+                case SendInputAction.Hold:
+                    InputControl.Hold(InputCode);
+                    break;
+
+                case SendInputAction.Release:
+                    InputControl.Release(InputCode);
+                    break;
+            }
+
+            return new ContinueResponse(ExitPorts[0].Destination);
+        }
+    }
+}
--- src/MacroModules.Model/Modules/ModuleType.cs
+++ src/MacroModules.Model/Modules/ModuleType.cs
@@ -2,6 +2,7 @@
 {
     public enum ModuleType
     {
+        SendInput,
         MoveCursor,
         PathCursor,
         Scroll,

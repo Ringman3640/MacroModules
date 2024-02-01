fbe39416953f21983dcdbd6539e37ef2392522c4
Franz Alarcon <franzalarconv@gmail.com>
2024-01-31 21:29:42 -0800

    Add primary IResponseValue implementations

---
+++ src/MacroModules.Model/Modules/Responses/ValuedContinueResponse.cs
@@ -0,0 +1,17 @@
+﻿using MacroModules.Model.Values;
+
+namespace MacroModules.Model.Modules.Responses
+{
+    /// <summary>
+    /// Represents a return message from a <see cref="Module"/> that provides a return
+    /// <see cref="Value"/> and indicates that the caller should execute the next indicated
+    /// <see cref="Module"/>.
+    /// </summary>
+    public class ValuedContinueResponse : ContinueResponse, IResponseValue
+    {
+        /// <inheritdoc/>
+        public Value? ReturnValue { get; set; } = null;
+
+        public ValuedContinueResponse(Module nextModule) : base(nextModule) { }
+    }
+}
---
+++ src/MacroModules.Model/Modules/Responses/ValuedEndResponse.cs
@@ -0,0 +1,14 @@
+﻿using MacroModules.Model.Values;
+
+namespace MacroModules.Model.Modules.Responses
+{
+    /// <summary>
+    /// Represents a return message from a <see cref="Module"/> that provides a return
+    /// <see cref="Value"/> and indicates that this is the last <see cref="Module"/> in the current
+    /// execution thread.
+    /// </summary>
+    public class ValuedEndResponse : EndResponse, IResponseValue
+    {
+        public Value? ReturnValue { get; set; } = null;
+    }
+}

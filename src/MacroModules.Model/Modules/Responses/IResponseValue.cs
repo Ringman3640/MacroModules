547a9e1cf324b750185b11d03dc3fb7eab038e2a
Franz Alarcon <franzalarconv@gmail.com>
2024-01-31 21:18:04 -0800

    Add IResponseValue

---
+++ src/MacroModules.Model/Modules/Responses/IResponseValue.cs
@@ -0,0 +1,15 @@
+﻿using MacroModules.Model.Values;
+
+namespace MacroModules.Model.Modules.Responses
+{
+    /// <summary>
+    /// Represents a return message from a <see cref="Module"/> that provides a <see cref="Value"/>.
+    /// </summary>
+    public interface IResponseValue
+    {
+        /// <summary>
+        /// Indicates the <see cref="Value"/> returned by the <see cref="Module"/>.
+        /// </summary>
+        public Value? ReturnValue { get; set; }
+    }
+}

3be9d1167546ee4d1a47a73703a7e5d7464e70e6
Franz Alarcon <franzalarconv@gmail.com>
2024-02-01 10:42:43 -0800

    Add ValuedModule specialization class

---
+++ src/MacroModules.Model/Modules/ValuedModule.cs
@@ -0,0 +1,21 @@
+﻿using MacroModules.Model.Modules.Responses;
+using MacroModules.Model.Values;
+
+namespace MacroModules.Model.Modules
+{
+    /// <summary>
+    /// Represents a specialized <see cref="Module"/> that returns a <see cref="Value"/> from its
+    /// execution <see cref="IResponse"/>.
+    /// </summary>
+    public abstract class ValuedModule : Module
+    {
+        /// <summary>
+        /// Gets the <see cref="ValueType"/> of the <see cref="Value"/> returned by this object from
+        /// <see cref="Execute(ref object?)"/>.
+        /// </summary>
+        public abstract ValueType ReturnValueType { get; }
+
+        /// <inheritdoc/>
+        public abstract override IResponse Execute(ref object? processData);
+    }
+}

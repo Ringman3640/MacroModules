123dd037a44108f1fe5df52c5790d9de849edcd4
Franz Alarcon <franzalarconv@gmail.com>
2024-02-01 21:08:09 -0800

    Implement PositionValue

---
+++ src/MacroModules.Model/Values/PositionValue.cs
@@ -0,0 +1,23 @@
+﻿using MacroModules.MacroLibrary.Types;
+
+namespace MacroModules.Model.Values
+{
+    public class PositionValue : Value, IValueData<Position>
+    {
+        public override ValueDataType Type { get; } = ValueDataType.Position;
+
+        public Position Data { get; set; }
+
+        public PositionValue() { }
+
+        public PositionValue(Position positionData)
+        {
+            Data = positionData;
+        }
+
+        public override Value Clone()
+        {
+            return new PositionValue(Data);
+        }
+    }
+}

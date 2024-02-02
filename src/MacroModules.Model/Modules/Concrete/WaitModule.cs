bebbbbc2cdac0827d785ae779a15dbb10268df63
Franz Alarcon <franzalarconv@gmail.com>
2024-02-02 09:46:50 -0800

    Implement WaitModule

---
+++ src/MacroModules.Model/Modules/Concrete/WaitModule.cs
@@ -0,0 +1,33 @@
+﻿using MacroModules.Model.Modules.Responses;
+
+namespace MacroModules.Model.Modules.Concrete
+{
+    /// <summary>
+    /// Represents a <see cref="Module"/> that pauses macro execution for a specified length of
+    /// time.
+    /// </summary>
+    public class WaitModule : Module
+    {
+        /// <summary>
+        /// Indicates the amount of time to wait for.
+        /// </summary>
+        public TimeSpan Time { get; set; } = TimeSpan.FromMilliseconds(500);
+
+        public override ModuleType Type { get; } = ModuleType.Wait;
+
+        public override void Initialize(out object? processData)
+        {
+            processData = false;
+        }
+
+        public override IResponse Execute(ref object? processData)
+        {
+            if (!(bool)processData!)
+            {
+                processData = true;
+                return new WaitRepeatResponse(Time);
+            }
+            return new ContinueResponse();
+        }
+    }
+}

ac48daf6cf3f7c2c97f9823b6d84a19549d6c5ca
Franz Alarcon <franzalarconv@gmail.com>
2024-02-02 16:21:49 -0800

    Implement GetPixelColorModule

---
+++ src/MacroModules.Model/Modules/Concrete/GetPixelColorModule.cs
@@ -0,0 +1,23 @@
+﻿using MacroModules.MacroLibrary;
+using MacroModules.MacroLibrary.Types;
+using MacroModules.Model.Modules.Responses;
+using MacroModules.Model.Values;
+using System.Drawing;
+
+namespace MacroModules.Model.Modules.Concrete
+{
+    public class GetPixelColorModule : ValuedModule
+    {
+        public Position PixelPosition { get; set; } = new();
+
+        public override ValueDataType ReturnValueType { get; } = ValueDataType.Color;
+
+        public override ModuleType Type { get; } = ModuleType.GetPixelColor;
+
+        public override IResponse Execute(ref object? processData)
+        {
+            Color pixelColor = ScreenCapture.GetPixelColor(PixelPosition);
+            return new ContinueResponse(new ColorValue(pixelColor));
+        }
+    }
+}

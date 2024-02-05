98acfddc5f5aa623678943159cb9948d52b644d8
Franz Alarcon <franzalarconv@gmail.com>
2024-02-05 15:40:36 -0800

    Add DurationValue

---
+++ src/MacroModules.Model/Values/DurationValue.cs
@@ -0,0 +1,31 @@
+﻿namespace MacroModules.Model.Values
+{
+    public class DurationValue : Value, IValueData<TimeSpan>
+    {
+        public override ValueDataType Type { get; protected set; } = ValueDataType.Duration;
+
+        public TimeSpan Data { get; set; }
+
+        public DurationValue()
+        {
+            Data = TimeSpan.FromSeconds(1);
+        }
+
+        public DurationValue(TimeSpan timeSpanData)
+        {
+            Data = timeSpanData;
+        }
+
+        public override Value Clone()
+        {
+            return new DurationValue(Data);
+        }
+
+        public override bool Equals(Value? other)
+        {
+            return base.Equals(other)
+                && other is DurationValue otherDuration
+                && Data.Equals(otherDuration.Data);
+        }
+    }
+}

e8cc3693f04434a2c045fe283a61a7dcb6d1015b
Franz Alarcon <franzalarconv@gmail.com>
2024-02-12 13:27:29 -0800

    Implement ElementAddedCommit and ElementRemovedCommit

---
+++ src/MacroModules.App/Managers/Commits/ElementAddedCommit.cs
@@ -0,0 +1,29 @@
+﻿using MacroModules.App.ViewModels;
+
+namespace MacroModules.App.Managers.Commits;
+
+public class ElementAddedCommit : Commit
+{
+    public ElementAddedCommit(ICommittable target, BoardElementVM element) : base(target)
+    {
+        this.element = element;
+    }
+
+    protected override void UndoAction()
+    {
+        if (target is ModuleBoardVM moduleBoard)
+        {
+            moduleBoard.RemoveElement(element);
+        }
+    }
+
+    protected override void RedoAction()
+    {
+        if (target is ModuleBoardVM moduleBoard)
+        {
+            moduleBoard.AddElement(element);
+        }
+    }
+
+    private readonly BoardElementVM element;
+}
---
+++ src/MacroModules.App/Managers/Commits/ElementRemovedCommit.cs
@@ -0,0 +1,29 @@
+﻿using MacroModules.App.ViewModels;
+
+namespace MacroModules.App.Managers.Commits;
+
+public class ElementRemovedCommit : Commit
+{
+    public ElementRemovedCommit(ICommittable target, BoardElementVM element) : base(target)
+    {
+        this.element = element;
+    }
+
+    protected override void UndoAction()
+    {
+        if (target is ModuleBoardVM moduleBoard)
+        {
+            moduleBoard.AddElement(element);
+        }
+    }
+
+    protected override void RedoAction()
+    {
+        if (target is ModuleBoardVM moduleBoard)
+        {
+            moduleBoard.RemoveElement(element);
+        }
+    }
+
+    private readonly BoardElementVM element;
+}

89367bb559a63017f3c447f32f5175a54c065656
Franz Alarcon <franzalarconv@gmail.com>
2024-02-12 15:04:07 -0800

    Add EntryModule

---
+++ src/MacroModules.Model/Modules/EntryModule.cs
@@ -0,0 +1,22 @@
+﻿using MacroModules.Model.Modules.Responses;
+
+namespace MacroModules.Model.Modules
+{
+    /// <summary>
+    /// Represents a specialized <see cref="Module"/> that indicates the start of a macro.
+    /// </summary>
+    public abstract class EntryModule : Module
+    {
+        /// <inheritdoc/>
+        public override bool IsConnectable { get; } = false;
+
+        /// <returns>
+        /// A <see cref="ContinueResponse"/> that points to the <see cref="ExitPort"/> at index 0.
+        /// </returns>
+        /// <inheritdoc/>
+        public override sealed IResponse Execute(ref object? processData)
+        {
+            return new ContinueResponse();
+        }
+    }
+}

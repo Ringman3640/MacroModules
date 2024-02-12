712cef77aefb292eabf541defc396c3d10243047
Franz Alarcon <franzalarconv@gmail.com>
2024-02-12 10:28:13 -0800

    Add base BoardElement and Label derived type

---
+++ src/MacroModules.Model/BoardElements/BoardElement.cs
@@ -0,0 +1,28 @@
+﻿namespace MacroModules.Model.BoardElements
+{
+    /// <summary>
+    /// Represents and element on a board that has a coordinate position relative to the board.
+    /// </summary>
+    public abstract class BoardElement
+    {
+        /// <summary>
+        /// Indicates the horizontal position of the element.
+        /// </summary>
+        /// <remarks>
+        /// A value of 0 represents a <see cref="BoardElement"/> that's positioned to the left edge
+        /// of the board. Positive values indicate a position to the right while negative values
+        /// indicate a position to the left.
+        /// </remarks>
+        public double PositionX { get; set; }
+
+        /// <summary>
+        /// Indicates the vertical position of the element.
+        /// </summary>
+        /// <remarks>
+        /// A value of 0 represents a <see cref="BoardElement"/> that's positioned to the top edge
+        /// of the board. Positive values indicate a position downwards while negative values
+        /// indicate a position upwards.
+        /// </remarks>
+        public double PositionY { get; set; }
+    }
+}
---
+++ src/MacroModules.Model/BoardElements/Label.cs
@@ -0,0 +1,13 @@
+﻿namespace MacroModules.Model.BoardElements
+{
+    /// <summary>
+    /// Represents a <see cref="BoardElement"/> that defines test on the board.
+    /// </summary>
+    public class Label : BoardElement
+    {
+        /// <summary>
+        /// Indicates the string test of the <see cref="Label"/>.
+        /// </summary>
+        public string Text { get; set; } = "";
+    }
+}

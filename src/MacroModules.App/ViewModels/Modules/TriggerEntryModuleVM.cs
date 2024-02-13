83eb3cbaf561e10bde46014aef8e151b18f2ff51
Franz Alarcon <franzalarconv@gmail.com>
2024-02-12 22:18:33 -0800

    Add Trigger property

--- src/MacroModules.App/ViewModels/Modules/TriggerEntryModuleVM.cs
+++ src/MacroModules.App/ViewModels/Modules/TriggerEntryModuleVM.cs
@@ -1,8 +1,15 @@
-﻿using MacroModules.Model.Modules;
+﻿using MacroModules.Model.Execution;
+using MacroModules.Model.Modules;
+using MacroModules.Model.Modules.Concrete;
 
 namespace MacroModules.App.ViewModels.Modules;
 
 public class TriggerEntryModuleVM : ModuleVM
 {
     public override ModuleType Type { get; } = ModuleType.TriggerEntry;
+
+    public InputTrigger? Trigger
+    {
+        get { return ((TriggerEntryModule)ModuleData).Trigger; }
+    }
 }

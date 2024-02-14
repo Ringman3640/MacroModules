fce09b241a2534c2f6587baf0658bac0c7d7bf27
Franz Alarcon <franzalarconv@gmail.com>
2024-02-14 11:16:31 -0800

    Expose model properties

--- src/MacroModules.App/ViewModels/Modules/MoveCursorModuleVM.cs
+++ src/MacroModules.App/ViewModels/Modules/MoveCursorModuleVM.cs
@@ -1,13 +1,48 @@
-﻿using MacroModules.Model.Modules;
+﻿using MacroModules.MacroLibrary.Types;
+using MacroModules.Model.Modules;
 using MacroModules.Model.Modules.Concrete;
 
 namespace MacroModules.App.ViewModels.Modules;
 
 public class MoveCursorModuleVM : ModuleVM
 {
+    public Position TargetPosition
+    {
+        get { return castedModuleData.TargetPosition; }
+        set
+        {
+            if (castedModuleData.TargetPosition != value)
+            {
+                castedModuleData.TargetPosition = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public TimeSpan TransitionTime
+    {
+        get { return castedModuleData.TransitionTime; }
+        set
+        {
+            if (castedModuleData.TransitionTime != value)
+            {
+                castedModuleData.TransitionTime = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
     public override ModuleType Type { get; } = ModuleType.MoveCursor;
 
-    public MoveCursorModuleVM() : base() { }
+    public MoveCursorModuleVM() : base()
+    {
+        castedModuleData = (MoveCursorModule)ModuleData;
+    }
 
-    public MoveCursorModuleVM(MoveCursorModule moduleData) : base(moduleData) { }
+    public MoveCursorModuleVM(MoveCursorModule moduleData) : base(moduleData)
+    {
+        castedModuleData = moduleData;
+    }
+
+    private readonly MoveCursorModule castedModuleData;
 }

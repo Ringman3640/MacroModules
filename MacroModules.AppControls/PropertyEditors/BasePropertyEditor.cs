d811b10c900c29c790ba9b7ec88a097447442ed1
Franz Alarcon <franzalarconv@gmail.com>
2024-02-14 19:59:07 -0800

    Start Position property editor

---
+++ MacroModules.AppControls/PropertyEditors/BasePropertyEditor.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MacroModules.AppControls.PropertyEditors;
+
+public partial class BasePropertyEditor : UserControl, INotifyPropertyChanged
+{
+    public static readonly DependencyProperty PropertyNameProperty =
+        DependencyProperty.Register("PropertyName", typeof(string), typeof(BasePropertyEditor), new PropertyMetadata(""));
+
+    public string PropertyName
+    {
+        get { return (string)GetValue(PropertyNameProperty); }
+        set { SetValue(PropertyNameProperty, value); }
+    }
+
+    public static readonly DependencyProperty PropertyDescriptionProperty =
+        DependencyProperty.Register("PropertyDescription", typeof(string), typeof(BasePropertyEditor), new PropertyMetadata(""));
+
+    public string PropertyDescription
+    {
+        get { return (string)GetValue(PropertyDescriptionProperty); }
+        set { SetValue(PropertyDescriptionProperty, value); }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ MacroModules.AppControls/PropertyEditors/PositionPropertyEditor.xaml.cs
@@ -0,0 +1,78 @@
+﻿using MacroModules.MacroLibrary.Types;
+using System.Windows;
+
+namespace MacroModules.AppControls.PropertyEditors;
+
+public partial class PositionPropertyEditor : BasePropertyEditor
+{
+    public Position PositionProperty
+    {
+        get { return (Position)GetValue(PositionPropertyProperty); }
+        set { SetValue(PositionPropertyProperty, value); }
+    }
+
+    public static readonly DependencyProperty PositionPropertyProperty = DependencyProperty.Register(
+        nameof(PositionProperty),
+        typeof(Position),
+        typeof(PositionPropertyEditor),
+        new FrameworkPropertyMetadata(
+            default(Position),
+            FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+    public string EntryTextX
+    {
+        get { return PositionProperty.X.ToString(); }
+        set
+        {
+            int parsedXPos;
+            try
+            {
+                parsedXPos = int.Parse(value);
+            }
+            catch
+            {
+                return;
+            }
+            if (parsedXPos != PositionProperty.X)
+            {
+                PositionProperty = new(parsedXPos, PositionProperty.Y);
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(PositionProperty));
+            }
+        }
+    }
+
+    public string EntryTextY
+    {
+        get { return PositionProperty.Y.ToString(); }
+        set
+        {
+            int parsedYPos;
+            try
+            {
+                parsedYPos = int.Parse(value);
+            }
+            catch
+            {
+                return;
+            }
+            if (parsedYPos != PositionProperty.Y)
+            {
+                PositionProperty = new(PositionProperty.X, parsedYPos);
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(PositionProperty));
+            }
+        }
+    }
+
+    public PositionPropertyEditor()
+    {
+        InitializeComponent();
+    }
+
+    private void PositionPropertyEditor_Loaded(object sender, RoutedEventArgs e)
+    {
+        OnPropertyChanged(nameof(EntryTextX));
+        OnPropertyChanged(nameof(EntryTextY));
+    }
+}

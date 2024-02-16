3cf8cc81ee0e0e45a40c997d7416a10c3391bd4f
Franz Alarcon <franzalarconv@gmail.com>
2024-02-16 15:33:49 -0800

    Add PercentPropertyEditor

---
+++ MacroModules.AppControls/PropertyEditors/PercentPropertyEditor.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows;
+
+namespace MacroModules.AppControls.PropertyEditors;
+
+public partial class PercentPropertyEditor : BasePropertyEditor
+{
+    public double PercentProperty
+    {
+        get { return (double)GetValue(PercentPropertyProperty); }
+        set { SetValue(PercentPropertyProperty, value); }
+    }
+    public static readonly DependencyProperty PercentPropertyProperty = DependencyProperty.Register(
+        name: nameof(PercentProperty),
+        propertyType: typeof(double),
+        ownerType: typeof(PercentPropertyEditor),
+        typeMetadata: new FrameworkPropertyMetadata(
+            defaultValue: default(double),
+            flags: FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+    public double SliderValue
+    {
+        get { return PercentProperty * 100; }
+        set
+        {
+            if (PercentProperty != value)
+            {
+                PercentProperty = value / 100;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(PercentDisplay));
+            }
+        }
+    }
+
+    public string PercentDisplay
+    {
+        get { return Math.Round(PercentProperty * 100).ToString() + "%"; }
+    }
+
+    public PercentPropertyEditor()
+    {
+        InitializeComponent();
+    }
+
+    private void PercentPropertyEditor_Loaded(object sender, RoutedEventArgs e)
+    {
+        OnPropertyChanged(nameof(SliderValue));
+        OnPropertyChanged(nameof(PercentDisplay));
+    }
+}

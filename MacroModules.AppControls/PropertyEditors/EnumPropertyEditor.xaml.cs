0ef36fedbdd4087cbdcd1ec7b774a3c4e5b6067a
Franz Alarcon <franzalarconv@gmail.com>
2024-02-15 18:11:30 -0800

    Add EnumPropertyEditor

---
+++ MacroModules.AppControls/PropertyEditors/EnumPropertyEditor.xaml.cs
@@ -0,0 +1,60 @@
+﻿using MacroModules.Model.Types;
+using System.Windows;
+
+namespace MacroModules.AppControls.PropertyEditors;
+
+public partial class EnumPropertyEditor : BasePropertyEditor
+{
+    public Type EnumType
+    {
+        get { return (Type)GetValue(EnumTypeProperty); }
+        set { SetValue(EnumTypeProperty, value); }
+    }
+    public static readonly DependencyProperty EnumTypeProperty = DependencyProperty.Register(
+        name: nameof(EnumType),
+        propertyType: typeof(Type),
+        ownerType: typeof(EnumPropertyEditor),
+        typeMetadata: new PropertyMetadata(default(Type)));
+
+    public Enum EnumProperty
+    {
+        get { return (Enum)GetValue(EnumPropertyProperty); }
+        set { SetValue(EnumPropertyProperty, value); }
+    }
+    public static readonly DependencyProperty EnumPropertyProperty = DependencyProperty.Register(
+        name: nameof(EnumProperty),
+        propertyType: typeof(Enum),
+        ownerType: typeof(EnumPropertyEditor),
+        typeMetadata: new FrameworkPropertyMetadata(
+            defaultValue: default(Enum),
+            flags: FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+    public Array? EnumArray
+    {
+        get { return (EnumType == null) ? null : EnumType.GetEnumValues(); }
+    }
+
+    public Enum SelectedEnum
+    {
+        get { return EnumProperty; }
+        set
+        {
+            if (EnumProperty != value)
+            {
+                EnumProperty = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public EnumPropertyEditor()
+    {
+        InitializeComponent();
+    }
+
+    private void EnumPropertyEditor_Loaded(object sender, RoutedEventArgs e)
+    {
+        OnPropertyChanged(nameof(EnumArray));
+        OnPropertyChanged(nameof(SelectedEnum));
+    }
+}

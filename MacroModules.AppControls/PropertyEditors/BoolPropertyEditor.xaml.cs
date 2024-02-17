9e17907f3a4f33a4dfa301278e1eb17228b9dc92
Franz Alarcon <franzalarconv@gmail.com>
2024-02-16 19:03:48 -0800

    Add BoolPropertyEditor

---
+++ MacroModules.AppControls/PropertyEditors/BoolPropertyEditor.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+
+namespace MacroModules.AppControls.PropertyEditors
+{
+    public partial class BoolPropertyEditor : BasePropertyEditor
+    {
+        public bool BoolProperty
+        {
+            get { return (bool)GetValue(BoolPropertyProperty); }
+            set { SetValue(BoolPropertyProperty, value); }
+        }
+        public static readonly DependencyProperty BoolPropertyProperty = DependencyProperty.Register(
+            name: nameof(BoolProperty),
+            propertyType: typeof(bool),
+            ownerType: typeof(BoolPropertyEditor),
+            typeMetadata: new FrameworkPropertyMetadata(
+                defaultValue: false,
+                flags: FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public bool IsSet
+        {
+            get { return BoolProperty; }
+            set
+            {
+                if (BoolProperty != value)
+                {
+                    BoolProperty = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public BoolPropertyEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void BoolPropertyEditor_Loaded(object sender, RoutedEventArgs e)
+        {
+            OnPropertyChanged(nameof(IsSet));
+        }
+    }
+}

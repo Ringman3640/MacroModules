28cc3b4645ab75b424e3b63800e2cee9d0c009cf
Franz Alarcon <franzalarconv@gmail.com>
2024-02-17 20:23:42 -0800

    Add StringPropertyEditor

---
+++ MacroModules.AppControls/PropertyEditors/StringPropertyEditor.xaml.cs
@@ -0,0 +1,43 @@
+﻿using MacroModules.MacroLibrary.Types;
+using System.Windows;
+
+namespace MacroModules.AppControls.PropertyEditors;
+
+public partial class StringPropertyEditor : BasePropertyEditor
+{
+    public string StringProperty
+    {
+        get { return (string)GetValue(StringPropertyProperty); }
+        set { SetValue(StringPropertyProperty, value); }
+    }
+    public static readonly DependencyProperty StringPropertyProperty = DependencyProperty.Register(
+        name: nameof(StringProperty),
+        propertyType: typeof(string),
+        ownerType: typeof(StringPropertyEditor),
+        typeMetadata: new FrameworkPropertyMetadata(
+            defaultValue: "",
+            flags: FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+    public string StringEntry
+    {
+        get { return StringProperty; }
+        set
+        {
+            if (StringProperty != value)
+            {
+                StringProperty = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public StringPropertyEditor()
+    {
+        InitializeComponent();
+    }
+
+    private void StringPropertyEditor_Loaded(object sender, System.Windows.RoutedEventArgs e)
+    {
+        OnPropertyChanged(nameof(StringEntry));
+    }
+}

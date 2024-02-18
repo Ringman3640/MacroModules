9625d8cd83d90182d2873bf1e4ccdb154e17960f
Franz Alarcon <franzalarconv@gmail.com>
2024-02-17 19:49:53 -0800

    Add InputCodePropertyEditor

---
+++ MacroModules.AppControls/PropertyEditors/InputCodePropertyEditor.xaml.cs
@@ -0,0 +1,79 @@
+﻿using MacroModules.MacroLibrary;
+using MacroModules.MacroLibrary.Types;
+using System.Windows;
+
+namespace MacroModules.AppControls.PropertyEditors;
+
+public partial class InputCodePropertyEditor : BasePropertyEditor
+{
+    public ushort InputCodeProperty
+    {
+        get { return (ushort)GetValue(InputCodePropertyProperty); }
+        set { SetValue(InputCodePropertyProperty, value); }
+    }
+    public static readonly DependencyProperty InputCodePropertyProperty = DependencyProperty.Register(
+        name: nameof(InputCodeProperty),
+        propertyType: typeof(ushort),
+        ownerType: typeof(InputCodePropertyEditor),
+        typeMetadata: new FrameworkPropertyMetadata(
+            defaultValue: default(ushort),
+            flags: FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+    public string InputCodeDisplay
+    {
+        get
+        {
+            if (InputCodeProperty == 0)
+            {
+                return "No Input";
+            }
+            if (Enum.IsDefined(typeof(InputCode), InputCodeProperty))
+            {
+                return ((InputCode)InputCodeProperty).ToString();
+            }
+            return "Vk_" + InputCodeProperty;
+        }
+    }
+
+    public InputCodePropertyEditor()
+    {
+        InitializeComponent();
+    }
+
+    private void SetInput_Click(object sender, RoutedEventArgs e)
+    {
+        StartInputMonitor();
+    }
+
+    private void StartInputMonitor()
+    {
+        InputMonitor.FilterInjectedInputs = true;
+        InputMonitor.FilterMouseMovements = true;
+        InputMonitor.SetInputHandler(InputMonitor_HandleInput);
+        InputMonitor.Install();
+        InputMonitor.CollectInput = true;
+    }
+
+    private void StopInputMonitor()
+    {
+        InputMonitor.CollectInput = false;
+        InputMonitor.Uninstall();
+    }
+
+    private void InputCodePropertyEditor_Loaded(object sender, RoutedEventArgs e)
+    {
+        OnPropertyChanged(nameof(InputCodeDisplay));
+    }
+
+    private bool InputMonitor_HandleInput(InputData input)
+    {
+        if (input.Type != InputType.KeyDown && input.Type != InputType.MouseDown)
+        {
+            return true;
+        }
+        InputCodeProperty = input.InputKeyCode;
+        StopInputMonitor();
+        OnPropertyChanged(nameof(InputCodeDisplay));
+        return false;
+    }
+}

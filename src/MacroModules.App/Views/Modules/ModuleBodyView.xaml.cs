82d08a4f668d5be682dd042b7f18d91cbed32565
Franz Alarcon <franzalarconv@gmail.com>
2024-02-19 16:12:22 -0800

    Separate module body component to own view

---
+++ src/MacroModules.App/Views/Modules/ModuleBodyView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MacroModules.App.Views.Modules;
+
+public partial class ModuleBodyView : UserControl
+{
+    public CornerRadius CornerRadius
+    {
+        get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+        set { SetValue(CornerRadiusProperty, value); }
+    }
+    public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register(
+        name: nameof(CornerRadius),
+        propertyType: typeof(CornerRadius),
+        ownerType: typeof(ModuleBodyView),
+        typeMetadata: new PropertyMetadata(default(CornerRadius)));
+
+    public ModuleBodyView()
+    {
+        InitializeComponent();
+    }
+}

b76eadd38e5bd2b6adc5d84a0d5ff9bab6b15e80
Franz Alarcon <franzalarconv@gmail.com>
2024-02-20 10:23:58 -0800

    Add VariableVM

---
+++ src/MacroModules.App/ViewModels/VariableVM.cs
@@ -0,0 +1,67 @@
+﻿using MacroModules.App.Managers;
+using MacroModules.App.Managers.Commits;
+using MacroModules.Model.Values;
+using MacroModules.Model.Variables;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MacroModules.App.ViewModels;
+
+public class VariableVM : ICommittable, INotifyPropertyChanged
+{
+    public WorkspaceVM Workspace { get; private set; }
+
+    public Variable VariableData { get; private set; }
+
+    public Guid Id
+    {
+        get { return VariableData.Id; }
+    }
+
+    public ValueDataType Type
+    {
+        get { return VariableData.Type; }
+    }
+
+    public string Name
+    {
+        get { return VariableData.Name; }
+        set
+        {
+            if (VariableData.Name != value)
+            {
+                FullCommitPropertyChange(VariableData.Name, value);
+                VariableData.Name = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public Value InitialValue
+    {
+        get { return VariableData.InitialValue; }
+    }
+
+    public bool PerformingCommitAction { get; set; }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public VariableVM(ValueDataType type, WorkspaceVM workspace)
+    {
+        VariableData = new(type);
+        Workspace = workspace;
+    }
+
+    private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private void FullCommitPropertyChange(object? originalValue, object newValue, [CallerMemberName] string? propertyName = null)
+    {
+        if (!PerformingCommitAction)
+        {
+            Workspace.CommitManager.PushToSeries(new PropertyCommit(this, propertyName!, originalValue, newValue));
+        }
+    }
+}

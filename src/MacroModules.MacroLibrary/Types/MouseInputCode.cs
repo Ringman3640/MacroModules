d6e1f8b43ca8df2a9efce0fdc3dfba978d65a5b8
Franz Alarcon <franzalarconv@gmail.com>
2024-02-24 22:06:53 -0800

    Add enum subset of InputControl for mouse inputs

---
+++ src/MacroModules.MacroLibrary/Types/MouseInputCode.cs
@@ -0,0 +1,18 @@
+﻿namespace MacroModules.MacroLibrary.Types
+{
+    /// <summary>
+    /// The set of Windows virtual key codes for mouse buttons mapped to their corresponding button
+    /// name.
+    /// </summary>
+    /// <remarks>
+    /// This enum set is a subset of <see cref="InputCode"/>.
+    /// </remarks>
+    public enum MouseInputCode
+    {
+        MouseLeft = 0x01,
+        MouseRight = 0x02,
+        MouseMiddle = 0x04,
+        MouseX1 = 0x05,
+        MouseX2 = 0x06,
+    }
+}

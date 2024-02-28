2a63a28e3422dd43e71ac51134b5a00d9e355599
Franz Alarcon <franzalarconv@gmail.com>
2024-02-27 22:40:11 -0800

    Add base design for TriggerEntryModule

---
+++ src/MacroModules.App/Views/Modules/TriggerEntryModuleView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace MacroModules.App.Views.Modules;
+
+public partial class TriggerEntryModuleView : UserControl
+{
+    public TriggerEntryModuleView()
+    {
+        InitializeComponent();
+    }
+}

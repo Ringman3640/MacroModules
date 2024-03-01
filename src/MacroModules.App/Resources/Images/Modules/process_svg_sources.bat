187ac47fe64240bd7f19e86f19f66c8aaa58dc8e
Franz Alarcon <franzalarconv@gmail.com>
2024-03-01 12:58:40 -0800

    Refactor theme key naming conventions

--- src/MacroModules.App/Resources/Images/Modules/process_svg_sources.bat
+++ src/MacroModules.App/Resources/Images/Modules/process_svg_sources.bat
@@ -1,3 +1,3 @@
 .\SvgToXaml\SvgToXaml.exe BuildDict /inputdir:.\SvgSource /outputname:ModuleIcons /outputdir:. /buildhtmlfile:false
 
-powershell -Command "(Get-Content ModuleIcons.xaml) -replace '#FF000000', '{StaticResource StandardModuleIconColor}' | Out-File -encoding ASCII ModuleIcons.xaml"
+powershell -Command "(Get-Content ModuleIcons.xaml) -replace '#FF000000', '{StaticResource Module.Icon.Color}' | Out-File -encoding ASCII ModuleIcons.xaml"

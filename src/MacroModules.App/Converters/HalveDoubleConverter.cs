c3cb535f57484bb3282557129c6cb763d53a2485
Franz Alarcon <franzalarconv@gmail.com>
2024-03-02 00:36:21 -0800

    Add style for form Buttons

---
+++ src/MacroModules.App/Converters/HalveDoubleConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace MacroModules.App.Converters;
+
+public class HalveDoubleConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return (double)value / 2; 
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return (double)value * 2;
+    }
+}

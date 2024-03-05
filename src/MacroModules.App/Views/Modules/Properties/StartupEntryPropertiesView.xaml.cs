0ebb9f3a1b69045261bbae1a0a2ed795b7c2a664
Franz Alarcon <franzalarconv@gmail.com>
2024-03-05 12:13:34 -0800

    Add empty property viewer for StartupEntryModuleVM

---
+++ src/MacroModules.App/Views/Modules/Properties/StartupEntryPropertiesView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace MacroModules.App.Views.Modules.Properties;
+
+public partial class StartupEntryPropertiesView : UserControl
+{
+    public StartupEntryPropertiesView()
+    {
+        InitializeComponent();
+    }
+}

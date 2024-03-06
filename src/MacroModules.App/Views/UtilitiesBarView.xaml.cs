564aab173424e60cd15af1187278a53aa8f0f738
Franz Alarcon <franzalarconv@gmail.com>
2024-03-05 21:57:03 -0800

    Add automation peer for UtilitiesBarView

--- src/MacroModules.App/Views/UtilitiesBarView.xaml.cs
+++ src/MacroModules.App/Views/UtilitiesBarView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows.Automation.Peers;
+using System.Windows.Controls;
 
 namespace MacroModules.App.Views;
 
@@ -7,5 +8,25 @@ public partial class UtilitiesBarView : UserControl
     public UtilitiesBarView()
     {
         InitializeComponent();
+    }
+
+    protected override AutomationPeer OnCreateAutomationPeer()
+    {
+        return new UtilitiesBarAutomationPeer(this);
+    }
+}
+
+public class UtilitiesBarAutomationPeer : UserControlAutomationPeer
+{
+    public UtilitiesBarAutomationPeer(UserControl owner) : base(owner) { }
+
+    protected override AutomationControlType GetAutomationControlTypeCore()
+    {
+        return AutomationControlType.Pane;
+    }
+
+    protected override string GetClassNameCore()
+    {
+        return nameof(UtilitiesBarView);
     }
 }

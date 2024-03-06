2bd6ae94febc276fefc82ade5ecf278ad7691fe1
Franz Alarcon <franzalarconv@gmail.com>
2024-03-05 22:14:00 -0800

    Add automation peer for ModuleBoardView

--- src/MacroModules.App/Views/ModuleBoardView.xaml.cs
+++ src/MacroModules.App/Views/ModuleBoardView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows.Automation.Peers;
+using System.Windows.Controls;
 
 namespace MacroModules.App.Views;
 
@@ -7,5 +8,25 @@ public partial class ModuleBoardView : UserControl
     public ModuleBoardView()
     {
         InitializeComponent();
+    }
+
+    protected override AutomationPeer OnCreateAutomationPeer()
+    {
+        return new ModuleBoardAutomationPeer(this);
+    }
+}
+
+public class ModuleBoardAutomationPeer : UserControlAutomationPeer
+{
+    public ModuleBoardAutomationPeer(UserControl owner) : base(owner) { }
+
+    protected override AutomationControlType GetAutomationControlTypeCore()
+    {
+        return AutomationControlType.Pane;
+    }
+
+    protected override string GetClassNameCore()
+    {
+        return nameof(ModuleBarView);
     }
 }

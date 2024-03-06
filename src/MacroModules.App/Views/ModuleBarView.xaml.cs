041499114e114eabedd5500701116692547a0237
Franz Alarcon <franzalarconv@gmail.com>
2024-03-05 21:31:28 -0800

    Add custom AutomationPeer for ModuleBarView

--- src/MacroModules.App/Views/ModuleBarView.xaml.cs
+++ src/MacroModules.App/Views/ModuleBarView.xaml.cs
@@ -1,11 +1,35 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows.Automation.Peers;
+using System.Windows.Controls;
 
 namespace MacroModules.App.Views;
 
 public partial class ModuleBarView : UserControl
 {
+    public ItemsControl ModuleBarItemsContainer { get; }
+
     public ModuleBarView()
     {
         InitializeComponent();
+        ModuleBarItemsContainer = moduleBarItemsContainer;
+    }
+
+    protected override AutomationPeer OnCreateAutomationPeer()
+    {
+        return new ModuleBarAutomationPeer(this);
+    }
+}
+
+public class ModuleBarAutomationPeer : UserControlAutomationPeer
+{
+    public ModuleBarAutomationPeer(UserControl owner) : base(owner) { }
+
+    protected override AutomationControlType GetAutomationControlTypeCore()
+    {
+        return AutomationControlType.Pane;
+    }
+
+    protected override string GetClassNameCore()
+    {
+        return nameof(ModuleBarView);
     }
 }
